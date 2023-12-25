34e01d8a762273c2fd39a2b3c2051cfd16dead99
Zaher Madi <zahermadi@yahoo.fr>
2023-12-25 20:05:06 +0100

    first commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Concessionnaire(models.Model):
+    nom = models.CharField(max_length=100)
+    numero_siret = models.IntegerField()
+    code_postal = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.nom
+
+class Voiture(models.Model):
+    marque = models.CharField(max_length=100)
+    modele = models.CharField(max_length=100)
+    chevaux = models.IntegerField()
+    concessionnaire = models.ForeignKey(Concessionnaire, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.marque} {self.modele}"
